ad576eec65233db09ce88d94d699cd7d3c7b25b8
Jesús Suárez Molina <jesuss@apolo.local>
2019-02-01 16:39:44 -0300

    Simulate messages.

--- src/app/chat-conversation/chat-conversation.component.ts
+++ src/app/chat-conversation/chat-conversation.component.ts
@@ -12,6 +12,33 @@ export class ChatConversationComponent implements OnInit {
   @Input() data: Observable<string>;
   chatId: string;
 
+  public messageList: Message[] = [
+    {
+      id: 1,
+      date: new Date(),
+      sentByMe: true,
+      message: 'Mensaje 1'
+    },
+    {
+      id: 2,
+      date: new Date(),
+      sentByMe: true,
+      message: 'Mensaje 2'
+    },
+    {
+      id: 3,
+      date: new Date(),
+      sentByMe: false,
+      message: 'Mensaje 3'
+    },
+
+  ];
+
+  /**
+   * Message to sent.
+   */
+  public messageToSent = '';
+
   constructor() { }
 
   ngOnInit() {
@@ -22,4 +49,37 @@ export class ChatConversationComponent implements OnInit {
     });
   }
 
+  /**
+   * SendMessage.
+   */
+  public sendMessage() {
+    console.log(`${ChatConversationComponent.name} sendMessage`);
+
+    this.messageList.push({
+      id: Math.random(),
+      date: new Date(),
+      message: this.messageToSent,
+      sentByMe: true
+    });
+
+    this.messageToSent = '';
+  }
+
+  /**
+   * getClassMessage
+   */
+  public getClassMessage(item) {
+    return item.sentByMe ? 'to' : 'from';
+  }
+
+}
+
+/**
+ * Message entity.
+ */
+interface Message {
+  id: number;
+  message: string;
+  sentByMe: boolean;
+  date: Date;
 }
